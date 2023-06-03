f56477d1daa5f16c0a63d4b2d213bc48c360b54e
petergpt <130558610+petergpt@users.noreply.github.com>
2023-06-03 23:36:38 +0000

    removed button

--- app.py
+++ app.py
@@ -21,8 +21,11 @@ def main():
     scientists_list = list(SCIENTISTS.keys())
     reasoning_list = list(REASONING.keys())
 
-    st.subheader("Choose a Philosopher, Funlosopher, Scientist or a Reasoning Method")
+    st.subheader("Choose a Philosopher, Funlosopher, Scientist, or Reasoning")
     option = st.radio("", ['Philosopher', 'Funlosopher', 'Scientist', 'Reasoning'])
+
+    thought_process = None
+    approach_description = None
 
     if option == 'Philosopher':
         selected_philosopher = st.selectbox("Select a Philosopher", philosophers_list)
@@ -35,50 +38,31 @@ def main():
         thought_process = SCIENTISTS.get(selected_philosopher, [])
     elif option == 'Reasoning':
         selected_philosopher = st.selectbox("Select a Reasoning", reasoning_list)
-        thought_process = REASONING.get(selected_philosopher, [])
+        selected_method = REASONING.get(selected_philosopher, {})
+        thought_process = selected_method.get("Thought_Process", [])
+        approach_description = selected_method.get("Approach", "")
     else:
         selected_philosopher = ""
         thought_process = []
 
     with st.form(key="ask_the_question_form"):
         user_question = st.text_input("Type your question here:")
-
+    
         submit_button = st.form_submit_button("Ask the Question")
-
+    
     if submit_button and selected_philosopher:
+        # Display the "Approach" paragraph
+        st.subheader(f"{selected_philosopher}'s Approach")
+        st.write(approach_description)
+
         with st.spinner("Progressing... Please wait"):
             api_response, final_answer = send_question_to_api(selected_philosopher, thought_process, user_question)
 
-        columns = st.beta_columns([2, 1])
-
-        with columns[0]:
-            st.subheader("Final Answer")
-            st.write(final_answer)
-
-        with columns[1]:
-            st.write("")  # Empty space to align the button with the title
-            st.write("""
-            <script>
-                function copyTextToClipboard(text) {
-                    var txtArea = document.createElement("textarea");
-                    txtArea.id = 'txt';
-                    txtArea.value = text;
-                    document.body.appendChild(txtArea);
-                    txtArea.select();
-                    document.execCommand('copy');
-                    document.body.removeChild(txtArea);
-                    document.getElementById('infoText').innerHTML = 'Copied!';
-                }
-            </script>
-            """, unsafe_allow_html=True)
-            escaped_final_answer = final_answer.replace("'", r"&#x27;").replace('\n', '\\n')
-            copy_button = f"""<button onclick="copyTextToClipboard('{escaped_final_answer}')">Copy to Clipboard</button>"""
-            copied_info = """<p id="infoText" style="font-size:small;"></p>"""
-            st.write(copy_button, unsafe_allow_html=True)
-            st.write(copied_info, unsafe_allow_html=True)
+        st.subheader("Final Answer")
+        st.write(final_answer)
 
         st.subheader(f"{selected_philosopher}'s Thought Process")
         st.write(api_response)
-      
+
 if __name__ == "__main__":
     main()
