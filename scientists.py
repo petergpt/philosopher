12a726c469651d4a78daa910811143cea99d4372
petergpt <130558610+petergpt@users.noreply.github.com>
2023-06-03 20:47:16 +0000

    format updated in scientists

--- scientists.py
+++ scientists.py
@@ -11,93 +11,111 @@ Kahneman = {
 }
 
 Copernicus_approach = {
-    'Approach': 'Observational analysis and mathematical reasoning.',
-    'Step 1': 'Identify problem: Determine the key issues or questions to be addressed.',
-    'Step 2': 'Collect data: Gather extensive empirical data related to the problem.',
-    'Step 3': 'Analyze data: Apply mathematical reasoning to analyze and interpret the data.',
-    'Step 4': 'Challenge established views: Dare to reinterpret the data in ways that challenge the established views.',
-    'Step 5': 'Develop theory: Create a theoretical framework based on your interpretation.',
-    'Conclude': 'Synthesize findings: Formulate a comprehensive understanding of the problem based on your theory.'
+    "Approach": "Observational analysis and mathematical reasoning.",
+    "Steps": [
+        {"1 - Identify the Problem": "Determine the key issues or questions to be addressed."},
+        {"2 - Collect Data": "Gather extensive empirical data related to the problem."},
+        {"3 - Analyze Data": "Apply mathematical reasoning to analyze and interpret the data."},
+        {"4 - Challenge Established Views": "Dare to reinterpret the data in ways that challenge the established views."},
+        {"5 - Develop Theory": "Create a theoretical framework based on your interpretation."},
+        {"6 - Conclude": "Synthesize findings: Formulate a comprehensive understanding of the problem based on your theory."}
+    ]
 }
 
 Darwin_approach = {
-    'Approach': 'Empirical research and theory of natural selection.',
-    'Step 1': 'Identify problem: Pinpoint the specific phenomenon or trend to be explained.',
-    'Step 2': 'Gather evidence: Collect observational and experimental data over time.',
-    'Step 3': 'Identify patterns: Look for patterns and trends within the collected data.',
-    'Step 4': 'Formulate theory: Develop a theory that can explain the patterns observed.',
-    'Step 5': 'Validate theory: Test the theory with additional observations and experiments.',
-    'Conclude': 'Finalize theory: Establish a theory that best fits with the collected evidence.'
+    "Approach": "Empirical research and theory of natural selection.",
+    "Steps": [
+        {"1 - Identify the Problem": "Pinpoint the specific phenomenon or trend to be explained."},
+        {"2 - Gather Evidence": "Collect observational and experimental data over time."},
+        {"3 - Identify Patterns": "Look for patterns and trends within the collected data."},
+        {"4 - Formulate Theory": "Develop a theory that can explain the patterns observed."},
+        {"5 - Validate Theory": "Test the theory with additional observations and experiments."},
+        {"6 - Conclude": "Finalize theory: Establish a theory that best fits with the collected evidence."}
+    ]
 }
 
 Einstein_approach = {
-    'Approach': 'Conceptual innovation and mathematical physics.',
-    'Step 1': 'Identify problem: Define the physical phenomenon to be explained.',
-    'Step 2': 'Conceptualize problem: Reconceptualize the problem based on theoretical understanding.',
-    'Step 3': 'Formulate equations: Develop mathematical equations to describe the phenomenon.',
-    'Step 4': 'Test predictions: Verify the predictions made by your mathematical models.',
-    'Step 5': 'Revise as needed: Update your models based on the outcome of your tests.',
-    'Conclude': 'Finalize model: Establish the model that best represents the physical reality.'
+    "Approach": "Conceptual innovation and mathematical physics.",
+    "Steps": [
+        {"1 - Identify the Problem": "Define the physical phenomenon to be explained."},
+        {"2 - Conceptualize Problem": "Reconceptualize the problem based on theoretical understanding."},
+        {"3 - Formulate Equations": "Develop mathematical equations to describe the phenomenon."},
+        {"4 - Test Predictions": "Verify the predictions made by your mathematical models."},
+        {"5 - Revise as Needed": "Update your models based on the outcome of your tests."},
+        {"6 - Conclude": "Finalize model: Establish the model that best represents the physical reality."}
+    ]
 }
 
 Curie_approach = {
-    'Approach': 'Empirical investigation and relentless experimentation.',
-    'Step 1': 'Identify problem: Choose a phenomenon in the natural world to investigate.',
-    'Step 2': 'Research background: Review existing literature to understand current knowledge and gaps.',
-    'Step 3': 'Experiment: Design and conduct rigorous experiments to test hypotheses.',
-    'Step 4': 'Data analysis: Analyze experimental data with mathematical precision.',
-    'Step 5': 'Theory development: Develop a theory based on the results of your experiments.',
-    'Conclude': 'Confirm theory: Validate your theory with further experiments and peer reviews.'
+    "Approach": "Empirical investigation and relentless experimentation.",
+    "Steps": [
+        {"1 - Identify the Problem": "Choose a phenomenon in the natural world to investigate."},
+        {"2 - Research Background": "Review existing literature to understand current knowledge and gaps."},
+        {"3 - Experiment": "Design and conduct rigorous experiments to test hypotheses."},
+        {"4 - Data Analysis": "Analyze experimental data with mathematical precision."},
+        {"5 - Theory Development": "Develop a theory based on the results of your experiments."},
+        {"6 - Conclude": "Confirm theory: Validate your theory with further experiments and peer reviews."}
+    ]
 }
 
 Newton_approach = {
-    'Approach': 'Mathematical principles and universal laws.',
-    'Step 1': 'Identify problem: Select a physical or mathematical problem to solve.',
-    'Step 2': 'Observation: Observe the phenomena related to the problem.',
-    'Step 3': 'Hypothesize: Formulate hypotheses based on observations.',
-    'Step 4': 'Experiment: Perform experiments to test the hypotheses.',
-    'Step 5': 'Mathematize: Use mathematical principles to describe the observations and experimental results.',
-    'Conclude': 'Establish laws: Derive universal laws or principles from the mathematical models.'
+    "Approach": "Mathematical principles and universal laws.",
+    "Steps": [
+        {"1 - Identify the Problem": "Select a physical or mathematical problem to solve."},
+        {"2 - Observation": "Observe the phenomena related to the problem."},
+        {"3 - Hypothesize": "Formulate hypotheses based on observations."},
+        {"4 - Experiment": "Perform experiments to test the hypotheses."},
+        {"5 - Mathematize": "Use mathematical principles to describe the observations and experimental results."},
+        {"6 - Conclude": "Establish laws: Derive universal laws or principles from the mathematical models."}
+    ]
 }
 
 Freud_approach = {
-    'Approach': 'Psychoanalysis and understanding of the unconscious mind.',
-    'Step 1': 'Identify problem: Identify a psychological or behavioral issue to address.',
-    'Step 2': 'Observation: Observe behaviors and reactions, both consciously and subconsciously.',
-    'Step 3': 'Analysis: Analyze these behaviors in relation to early experiences and unconscious thoughts.',
-    'Step 4': 'Theory formation: Formulate a theory that explains these behaviors and experiences.',
-    'Step 5': 'Validation: Validate the theory through further observation, therapy, and analysis.',
-    'Conclude': 'Synthesize findings: Formulate a comprehensive understanding of the issue based on your theory.'
+    "Approach": "Psychoanalysis and understanding of the unconscious mind.",
+    "Steps": [
+        {"1 - Identify the Problem": "Identify a psychological or behavioral issue to address."},
+        {"2 - Observation": "Observe behaviors and reactions, both consciously and subconsciously."},
+        {"3 - Analysis": "Analyze these behaviors in relation to early experiences and unconscious thoughts."},
+        {"4 - Theory Formation": "Formulate a theory that explains these behaviors and experiences."},
+        {"5 - Validation": "Validate the theory through further observation, therapy, and analysis."},
+        {"6 - Conclude": "Synthesize findings: Formulate a comprehensive understanding of the issue based on your theory."}
+    ]
 }
 
 Turing_approach = {
-    'Approach': 'Logical reasoning and computational thinking.',
-    'Step 1': 'Identify problem: Determine a complex problem requiring computational solutions.',
-    'Step 2': 'Conceptualize: Understand the problem in terms of logical and computational structures.',
-    'Step 3': 'Algorithm design: Design an algorithm to solve the problem.',
-    'Step 4': 'Test algorithm: Implement and test the algorithm to ensure it solves the problem.',
-    'Step 5': 'Refine: Refine the algorithm based on test results to increase efficiency.',
-    'Conclude': 'Finalize algorithm: Validate the final algorithm as a solution to the problem.'
+    "Approach": "Logical reasoning and computational thinking.",
+    "Steps": [
+        {"1 - Identify the Problem": "Determine a complex problem requiring computational solutions."},
+        {"2 - Conceptualize": "Understand the problem in terms of logical and computational structures."},
+        {"3 - Algorithm Design": "Design an algorithm to solve the problem."},
+        {"4 - Test Algorithm": "Implement and test the algorithm to ensure it solves the problem."},
+        {"5 - Refine": "Refine the algorithm based on test results to increase efficiency."},
+        {"6 - Conclude": "Finalize algorithm: Validate the final algorithm as a solution to the problem."}
+    ]
 }
 
 Tesla_approach = {
-    'Approach': 'Inventive thinking and experimentation.',
-    'Step 1': 'Identify problem: Identify a technical or engineering challenge.',
-    'Step 2': 'Ideation: Generate innovative solutions to the problem.',
-    'Step 3': 'Design: Design an experimental prototype based on the chosen solution.',
-    'Step 4': 'Prototype: Build a prototype and test its functionality.',
-    'Step 5': 'Refinement: Refine the design based on test results for optimal performance.',
-    'Conclude': 'Finalize design: Validate the final prototype as a solution to the problem.'
+    "Approach": "Inventive thinking and experimentation.",
+    "Steps": [
+        {"1 - Identify the Problem": "Identify a technical or engineering challenge."},
+        {"2 - Ideation": "Generate innovative solutions to the problem."},
+        {"3 - Design": "Design an experimental prototype based on the chosen solution."},
+        {"4 - Prototype": "Build a prototype and test its functionality."},
+        {"5 - Refinement": "Refine the design based on test results for optimal performance."},
+        {"6 - Conclude": "Finalize design: Validate the final prototype as a solution to the problem."}
+    ]
 }
 
 Hawking_approach = {
-    'Approach': 'Theoretical physics and cosmological insights.',
-    'Step 1': 'Identify problem: Determine a cosmological or theoretical physics problem to solve.',
-    'Step 2': 'Theoretical exploration: Develop mathematical models to describe the problem.',
-    'Step 3': 'Hypothesis formation: Formulate theories based on the mathematical models.',
-    'Step 4': 'Validation: Validate the theories through their implications and existing experimental data.',
-    'Step 5': 'Theory refinement: Refine the theories based on validation results.',
-    'Conclude': 'Finalize theory: Establish the theory that best explains the problem.'
+    "Approach": "Theoretical physics and cosmological insights.",
+    "Steps": [
+        {"1 - Identify the Problem": "Determine a cosmological or theoretical physics problem to solve."},
+        {"2 - Theoretical Exploration": "Develop mathematical models to describe the problem."},
+        {"3 - Hypothesis Formation": "Formulate theories based on the mathematical models."},
+        {"4 - Validation": "Validate the theories through their implications and existing experimental data."},
+        {"5 - Theory Refinement": "Refine the theories based on validation results."},
+        {"6 - Conclude": "Finalize theory: Establish the theory that best explains the problem."}
+    ]
 }
 
 SCIENTISTS = {
